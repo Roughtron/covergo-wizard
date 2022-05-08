1ed2655b9d2e7917ff65ede6ca58e15014d2aecd
Roughtron <roughtron@yandex.ru>
2022-05-08 20:33:23 +0300

    Add some styl variables

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      stylus: {
+        stylusOptions: {
+          import: [
+            path.resolve(__dirname, './src/styles/typography.styl'),
+            path.resolve(__dirname, './src/styles/colors.styl'),
+            path.resolve(__dirname, './src/styles/layout.styl'),
+            path.resolve(__dirname, './src/styles/shadows.styl')
+          ]
+        }
+      }
+    }
+  },
+  configureWebpack: {
+    resolve: {
+      extensions: ['.styl']
+    }
+  }
 })
